b7d24efc163021391480d4e5f033d5f095452281
Mike Moran <mike@houseofmoran.com>
2019-05-08 23:17:08 +0100

    introduce qrcode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #[macro_use]
 extern crate nickel;
 
+extern crate image;
+extern crate qrcode;
+
+use image::Luma;
+use qrcode::QrCode;
+
 use nickel::{HttpRouter, Nickel};
 
 fn main() {
@@ -10,6 +16,17 @@ fn main() {
         "/:message",
         middleware! { |request|
             let message = request.param("message").unwrap();
+
+            let code = QrCode::new(message).unwrap();
+
+            // Render the bits into an image.
+            let _image = code.render::<Luma<u8>>().build();
+
+            let string = code.render()
+                .light_color(' ')
+                .dark_color('#')
+                .build();
+            println!("{}", string);
 
             message
         },
