9e4683b294e483793ba0035f6dc90a9f1219699a
Mike Moran <mike@houseofmoran.com>
2019-05-08 22:34:31 +0100

    echo the message

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use nickel::{HttpRouter, Nickel};
 
 fn main() {
     let mut server = Nickel::new();
-    server.get("**", middleware!("Hello World"));
-    server.listen("127.0.0.1:6767");
+
+    server.get(
+        "/:message",
+        middleware! { |request|
+            let message = request.param("message").unwrap();
+
+            message
+        },
+    );
+
+    server.listen("127.0.0.1:6767").unwrap();
 }
