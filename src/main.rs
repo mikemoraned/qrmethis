de851d24222c26fe13257597c08e2b482bcfbd6b
Mike Moran <mike@houseofmoran.com>
2019-05-09 22:01:51 +0100

    add QRcode

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,27 @@
 #[macro_use]
 extern crate rocket;
 
+extern crate image;
+extern crate qrcode;
+
+use image::ImageBuffer;
+use image::Luma;
+use qrcode::QrCode;
+
 use rocket::http::RawStr;
 
 #[get("/<message>")]
-fn message(message: &RawStr) -> String {
-    format!("{}", message.as_str())
+fn message(message: &RawStr) -> ImageBuffer {
+    let code = QrCode::new(message).unwrap();
+
+    // Render the bits into an image.
+    let image = code.render::<Luma<u8>>().build();
+
+    let string = code.render().light_color(' ').dark_color('#').build();
+    println!("{}", string);
+
+    // format!("{}", message.as_str())
+    image
 }
 
 fn main() {
