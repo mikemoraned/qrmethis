cd2d53e1df044753412ac46a8ade3d25bdd40a83
Mike Moran <mike@houseofmoran.com>
2019-05-11 14:19:48 +0100

    pass buffer so that it can be written to

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ extern crate rocket;
 extern crate image;
 extern crate qrcode;
 
-//use image::{png, ColorType};
-// use image::Luma;
+
 use qrcode::QrCode;
 
 use rocket::http::RawStr;
@@ -20,14 +19,7 @@ use qrcode_response::QrCodeResponse;
 fn message(message: &RawStr) -> QrCodeResponse {
     let code = QrCode::new(message).unwrap();
 
-    //    // Render the bits into an image.
-    //    let image = code.render::<Luma<u8>>().build();
-    //
-    //    let bytes = vec![];
-    //    png::PNGEncoder::new(bytes).encode(&image, image.width(), image.height(), ColorType::Gray(1)).unwrap();
-
-    //    bytes
-    QrCodeResponse { qr_code: code }
+    QrCodeResponse::new(code)
 }
 
 fn main() {
