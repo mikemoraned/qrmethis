243c4580fb285e597f21a885043c04c5898098dd
Mike Moran <mike@houseofmoran.com>
2019-05-12 14:45:26 +0100

    simplify back down to one file

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,34 @@
 #[macro_use]
 extern crate rocket;
 
-extern crate image;
-extern crate qrcode;
+use std::io::Cursor;
 
-
+use image::Luma;
+use image::{png, ColorType};
 use qrcode::QrCode;
 
+
+use rocket::http::ContentType;
 use rocket::http::RawStr;
-
-mod qrcode_response;
-
-use qrcode_response::QrCodeResponse;
+use rocket::http::Status;
+use rocket::response::Response;
+use std::io::Write;
 
 #[get("/<message>")]
-fn message(message: &RawStr) -> QrCodeResponse {
-    let code = QrCode::new(message).unwrap();
+fn message(message: &RawStr) -> Result<Response, Status> {
+    let qr_code = QrCode::new(message).unwrap();
 
-    QrCodeResponse::new(code)
+    let image = qr_code.render::<Luma<u8>>().build();
+
+    let mut buffer = Vec::new();
+    png::PNGEncoder::new(buffer.by_ref())
+        .encode(&image, image.width(), image.height(), ColorType::Gray(8))
+        .map_err(|_| Status::BadRequest)?;
+
+    Response::build()
+        .header(ContentType::PNG)
+        .sized_body(Cursor::new(buffer))
+        .ok()
 }
 
 fn main() {
